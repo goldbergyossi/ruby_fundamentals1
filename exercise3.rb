bef066b895b7c49c508bd87625ca4bea9b417335
goldbergyossi <yygold@gmail.com>
2019-12-05 21:00:25 -0500

    2.0

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,7 @@
-puts "what's your name? "
+puts "what's your name? " 
+user_name = gets.chomp
+puts "Hello, #{user_name}!"
+puts "What year were you born in? "
+year = gets.chomp.to_i
+age = 2019 - year 
+puts "You are #{age} years old"
