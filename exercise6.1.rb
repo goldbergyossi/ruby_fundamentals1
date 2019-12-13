09b80a92790fbf2a10b15ed8b78407ecf38b6782
goldbergyossi <yygold@gmail.com>
2019-12-12 22:12:39 -0500

    1st assignment done

--- exercise6.1.rb
+++ exercise6.1.rb
@@ -1,6 +1,9 @@
 walk = 1
 run = 5
 hike = 0
+energy = 10
+eat = 10
+rest = 10
 puts "Do you want to walk or run home?? "
 input = gets.chomp
 
@@ -10,11 +13,17 @@ until input == "go home" do
     input = gets.chomp
     
     if input == "walk" 
-        hike += walk
+        hike += walk 
+        energy += 1
         puts "Your distance from home is #{hike}km"
     elsif input == "run"  
         hike += run 
+        energy -= 1
         puts "Your distance from home is #{hike}km" 
-         
+    elsif energy == 0 
+        puts "You don't have the energy to continue "
+    elsif input == "eat" || "rest"
+        puts "Your energy is restored"
     end 
 end 
+
